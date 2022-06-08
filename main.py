450230e4b9ad8514ec8acf4b273143218e37f5ab
WaspVentMan <66962696+WaspVentMan@users.noreply.github.com>
2022-06-09 00:20:45 +0100

    slight "improvements"

--- main.py
+++ main.py
@@ -1,13 +1,27 @@
 import json
 import time
-import os
 
 m = json.load(open("messages.json"))
 
 code = open(input("Enter the name of a .ps file to run.\n>>> ")+".ps", "r").read().split("\n")
-variables = {}; ln = 0; i = 0; ifDepth = 0; endIgnore = 0; elseHelper = False
+variables = {}; ln = 0; i = 0; ifDepth = 0; endIgnore = 0; elseHelper = False; whilePoint = 0
 
-def error(m, ew, id, ln, e1="", e2="", e3="", e4="", e5=""): print("[ERROR {}] Error on line {}, ".format(ew.upper()+str(id), str(ln)) + m[ew][id].format(e1, e2, e3, e4, e5))
+def error(m, ew, id, ln, *errs): print("[ERROR {}] Line {}, ".format(ew.upper()+str(id), str(ln)) + m[ew][id].format(", ".join(errs)))
+
+def iffy(line, variables):
+    line = line[i:].split(" ", 1)[1].replace("less than or equal to", "<=").replace("greater than or equal to", ">=").replace("less than", "<").replace("greater than", ">").replace("not equal to", "!=").replace("equal to", "==").replace(" is", "").split(" ")
+    if line[0].startswith("\"") and line[0].endswith("\""): line[0] = line[0][1:-1]
+    else:
+        try: line[0] = int(line[0])
+        except: line[0] = variables[line[0]]
+    if line[2].startswith("\"") and line[2].endswith("\""): line[2] = line[2][1:-1]
+    else:
+        try: line[2] = int(line[2])
+        except: line[2] = variables[line[2]]
+    if type(line[0]) == type("UUFO") or type(line[2]) == type("Mononon"): # 2021.07.11 // IoT Goddess
+        if line[1] in ["<=", ">=", "<", ">"]: error(m, "e", 5, ln, line[0] if type(line[0]) == type("C3390") else line[2]); exit() # CHÂTEAU
+    if line[1] == "<=" and line[0] <= line[2] or line[1] == ">=" and line[0] >= line[2] or line[1] == "<"  and line[0] <  line[2] or line[1] == ">"  and line[0] >  line[2] or line[1] == "!=" and line[0] != line[2] or line[1] == "==" and line[0] == line[2]: return True
+    else: return False
 
 while True:
     ln += 1
@@ -25,47 +39,16 @@ while True:
         except: error(m, "e", 3, ln); break
         if ifDepth == 0: error(m, "w", 1, ln); continue
         if line in ["if", "elif"]: ifDepth -= 1; elseHelper = False
+        elif line in ["while"]: ifDepth -= 1; elseHelper = False
         else: error(m, "e", 4, ln, line); break
 
     elif line[i:].startswith("if"):
-        line = line[i:].split(" ", 1)[1].replace("less than or equal to", "<=").replace("greater than or equal to", ">=").replace("less than", "<").replace("greater than", ">").replace("not equal to", "!=").replace("equal to", "==").replace(" is", "").split(" ")
-        if line[0].startswith("\"") and line[0].endswith("\""): line[0] = line[0][1:-1]
-        else:
-            try: line[0] = int(line[0])
-            except: line[0] = variables[line[0]]
-        if line[2].startswith("\"") and line[2].endswith("\""): line[2] = line[2][1:-1]
-        else:
-            try: line[2] = int(line[2])
-            except: line[2] = variables[line[2]]
-        if type(line[0]) == type("WYSI") or type(line[2]) == type("WYSI"): # WYSI -When You See It-
-            if line[1] in ["<=", ">=", "<", ">"]: error(m, "e", 5, ln); break
-        if   line[1] == "<=" and line[0] <= line[2]: ifDepth += 1; elseHelper = True
-        elif line[1] == ">=" and line[0] >= line[2]: ifDepth += 1; elseHelper = True
-        elif line[1] == "<"  and line[0] <  line[2]: ifDepth += 1; elseHelper = True
-        elif line[1] == ">"  and line[0] >  line[2]: ifDepth += 1; elseHelper = True
-        elif line[1] == "!=" and line[0] != line[2]: ifDepth += 1; elseHelper = True
-        elif line[1] == "==" and line[0] == line[2]: ifDepth += 1; elseHelper = True
+        if iffy(line, variables): ifDepth += 1; elseHelper = True
         else: endIgnore += 1
 
     elif line[i:].startswith("elif"):
         if elseHelper: ifDepth -= 1; continue
-        line = line[i:].split(" ", 1)[1].replace("less than or equal to", "<=").replace("greater than or equal to", ">=").replace("less than", "<").replace("greater than", ">").replace("not equal to", "!=").replace("equal to", "==").replace(" is", "").split(" ")
-        if line[0].startswith("\"") and line[0].endswith("\""): line[0] = line[0][1:-1]
-        else:
-            try: line[0] = int(line[0])
-            except: line[0] = variables[line[0]]
-        if line[2].startswith("\"") and line[2].endswith("\""): line[2] = line[2][1:-1]
-        else:
-            try: line[2] = int(line[2])
-            except: line[2] = variables[line[2]]
-        if type(line[0]) == type("WYSI") or type(line[2]) == type("WYSI"): # WYSI -When You See It-
-            if line[1] in ["<=", ">=", "<", ">"]: error(m, "e", 5, ln); break
-        if   line[1] == "<=" and line[0] <= line[2]: ifDepth += 1; endIgnore -= 1; elseHelper = True
-        elif line[1] == ">=" and line[0] >= line[2]: ifDepth += 1; endIgnore -= 1; elseHelper = True
-        elif line[1] == "<"  and line[0] <  line[2]: ifDepth += 1; endIgnore -= 1; elseHelper = True
-        elif line[1] == ">"  and line[0] >  line[2]: ifDepth += 1; endIgnore -= 1; elseHelper = True
-        elif line[1] == "!=" and line[0] != line[2]: ifDepth += 1; endIgnore -= 1; elseHelper = True
-        elif line[1] == "==" and line[0] == line[2]: ifDepth += 1; endIgnore -= 1; elseHelper = True
+        if iffy(line, variables): ifDepth += 1; endIgnore -= 1; elseHelper = True
         else: endIgnore += 1
 
     elif line[i:].startswith("else"):
@@ -96,10 +79,14 @@ while True:
 
     elif line[i:].startswith("change"):
         line = line[i:].split(" ", 1)[1].split(" by ")
+        try: test = variables[line[0]]
+        except: error(m, "e", 1, ln, line[0]); break
         try: variables[line[0]] += int(line[1])
         except: error(m, "e", 2, ln, line[1]); break
 
     elif line[i:].startswith("wait"):
         line = line[i:].split(" ", 1)[1]
-        try: time.sleep(int(line))
-        except: error(m, "e", 6, ln); break
+        try: line = variables[line]; time.sleep(line)
+        except:
+            try: time.sleep(int(line))
+            except: error(m, "e", 6, ln, str(line)); break
